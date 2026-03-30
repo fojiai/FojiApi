83744c6987974d8506ca1f58cda5ac31578e57b6
Mateus <mateuspalacio@gmail.com>
2026-03-30 13:29:12 -0300

    pls go

--- src/FojiApi.Web.API/Program.cs
+++ src/FojiApi.Web.API/Program.cs
@@ -64,6 +64,7 @@ var jwtSecret = builder.Configuration["Jwt:Secret"]
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
+        options.MapInboundClaims = false;
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
